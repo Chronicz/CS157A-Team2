68ae544e4efa67561f485213b7a44a893e97540c
Chronicz <45631752+Chronicz@users.noreply.github.com>
2024-07-02 19:57:06 -0700

    Backend SQL Connection boilerplate
    *SQL Database is local and remote is not enabled yet

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,26 @@
 import express from "express"
+import mysql from "mysql2"
+
 const app = express()
 
-app.listen(8080, ()=>{
-    console.log("Connected to backend (Express)")
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "jin",
+    password: "jin2000",
+    database: "cozyfirm"
 })
 
-app.get("/", (req, res)=>{
-    res.json("Home page")
+app.listen(8080, () => {
+    console.log("Connected to backend...")
+})
+
+app.get("/", (req, res) => {
+    const q = "SELECT * FROM cozyfirm.users;"
+    db.query(q, (err, data) => {
+        if (err) {
+            return res.json(err)
+        } else {
+            return res.json(data)
+        }
+    })
 })
