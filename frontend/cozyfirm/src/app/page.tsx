5147f03ae1e149460fd9a9c6c29c4f8590e98f6c
PikaPiperPickedaPickledPepper <kethyakas@yahoo.com>
2024-07-21 11:49:06 -0700

    Updated home page to display users

---
+++ frontend/cozyfirm/src/app/page.tsx
@@ -0,0 +1,61 @@
+"use client";
+
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+interface User {
+  user_id: number;
+  username: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+}
+
+const Home = () => {
+  const [users, setUsers] = useState<User[]>([]);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get("http://localhost:8000");
+        setUsers(res.data);
+        console.log(res);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1 className="font-bold text-3xl">INSIDE HOME</h1>
+      <div className="overflow-x-auto">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th className="py-3 px-6">Username</th>
+              <th className="py-3 px-6">First Name</th>
+              <th className="py-3 px-6">Last Name</th>
+            </tr>
+          </thead>
+          <tbody>
+            {users.map((user: User) => (
+              <tr
+                key={user.user_id}
+                className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
+              >
+                <td className="py-4 px-6">{user.username}</td>
+                <td className="py-4 px-6">{user.first_name}</td>
+                <td className="py-4 px-6">{user.last_name}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
