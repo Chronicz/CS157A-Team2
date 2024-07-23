cd5fe4c47c13e2450aaf83fbeb8a2d317fe105ae
Chronicz <45631752+Chronicz@users.noreply.github.com>
2024-07-22 21:35:48 -0700

    Added Navbar

---
+++ frontend/cozyfirm/components/Navbar.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link"
+import { NAV_LINKS } from "../constants"
+import Button from "../components/buttons";
+import Image from "next/image";
+const navbar = () => {
+    return (
+        <nav className="border-2 flexBetween max-container padding-container relative z-30 py-5">
+            <Link href="/" className="font-bold">
+                COZYFIRM
+            </Link>
+            <ul className="hidden h-full gap-12 lg:flex">
+                {NAV_LINKS.map((link) => (
+                    <Link href={link.href} key={link.key} className="font-bold flexCenter cursor-pointer pb-1.5 transition-all hover:font-extrabold">
+                        {link.label}
+                    </Link>
+                ))}
+            </ul>
+
+            <div className="lg:flexCenter hidden">
+                <Button
+                    type="button"
+                    title="Login"
+                    variant="btn_dark_green"
+                />
+            </div>
+
+            <Image
+                src="menu.svg"
+                alt="menu"
+                width={32}
+                height={32}
+                className="inline-block cursor-pointer lg:hidden"
+            />
+        </nav>
+    )
+}
+
+export default navbar
