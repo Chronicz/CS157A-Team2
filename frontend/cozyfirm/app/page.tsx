04bdfbd5ea663da3a58db9a6818836de28c7cdc8
RepiMinji <lancele195@gmail.com>
2024-07-23 19:42:42 -0700

    Update page.tsx
    added divs to page.tsx; error on line 15, not sure how to fix

--- frontend/cozyfirm/app/page.tsx
+++ frontend/cozyfirm/app/page.tsx
@@ -1,8 +1,9 @@
 "use client";
 
-import React from 'react'
-import { useEffect, useState } from 'react'
-import axios from 'axios'
+import React from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import buttons from "../components/buttons";
 
 const Home = () => {
   const [users, setUsers] = useState([]);
@@ -10,21 +11,54 @@ const Home = () => {
   useEffect(() => {
     const fetchUsers = async () => {
       try {
-        const res = await axios.get("http://localhost:8000")
-        setUsers(res.data)
-        console.log(res)
+        const res = await axios.get("http://localhost:8000");
+        setUsers(res.data);
+        console.log(res);
       } catch (err) {
-        console.log(err)
+        console.log(err);
       }
-    }
-    fetchUsers()
-  }, [])
+    };
+    fetchUsers();
+  }, []);
 
   return (
-    <div>
-      <h1>Hellow World</h1>
-    </div>
-  )
-}
+    <main className="my-44">
+      <h1 className="text-black text-3xl text-center">Account</h1>
+      <div>
+        <p className="text-black mx-16 font-bold text-lg">Hello (username)!</p>
+        <div className="my-16">
+          <div className="grid-container grid grid-cols-3 mx-96 border-2 p-2 border-black text-black text-lg">
+            <div className="grid-item mx-4 font-bold">Username</div>
+            <div className="grid-item mx-4"></div>
+            <div className="grid-item mx-4"></div>
+            <div className="grid-item mx-4">a username</div>
+            <div className="grid-item mx-4"></div>
+            <div className="grid-item mx-4"></div>
+            <div className="grid-item mx-4 my-4"></div>
+            <div className="grid-item mx-4 my-4"></div>
+            <div className="grid-item mx-4 my-4"></div>
+            <div className="grid-item mx-4 font-bold">First Name</div>
+            <div className="grid-item mx-4 font-bold">Last Name</div>
+            <div className="grid-item mx-4 font-bold">Password</div>
+            <div className="grid-item mx-4">a first name</div>
+            <div className="grid-item mx-4">a last name</div>
+            <div className="grid-item mx-4">a password</div>
+          </div>
+          <div className="flex row-wrap flex-end mx-96">
+            <div className="my-2">
+              <buttons type="button" title="Account Settings"></buttons>
+            </div>
+            <div className="p-2">
+              <button type="button" title="Wishlist"></button>
+            </div>
+            <div className="my-2">
+              <button type="button" title="My Blogs"></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default Home
+export default Home;
