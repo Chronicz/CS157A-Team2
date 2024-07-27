fc82892f834aa1ef339bc8238b62f2a23f3dc9a7
RepiMinji <lancele195@gmail.com>
2024-07-27 10:37:11 -0700

    Update page.tsx
    pulls user data from the database; working on functionality for censoring the password (not priority)

--- frontend/cozyfirm/app/page.tsx
+++ frontend/cozyfirm/app/page.tsx
@@ -5,6 +5,18 @@ import { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "../components/buttons";
 
+interface User {
+  user_id: number;
+  username: string;
+  password: string;
+  first_name: string;
+  last_name: string;
+}
+
+const censorPassword = (str: string): string =>{
+  return str.replace(/./g, "*");
+}
+
 const Home = () => {
   const [users, setUsers] = useState([]);
 
@@ -22,54 +34,59 @@ const Home = () => {
   }, []);
 
   return (
-    <main className="my-44">
-      <h1 className="text-black text-3xl text-center">Account</h1>
-      <div>
-        <p className="text-black mx-16 font-bold text-lg">Hello (username)!</p>
-        <div className="my-16">
-          <div className="grid-container grid grid-cols-3 mx-96 border-2 p-2 border-black text-black text-lg">
-            <div className="grid-item mx-4 font-bold">Username</div>
-            <div className="grid-item mx-4"></div>
-            <div className="grid-item mx-4"></div>
-            <div className="grid-item mx-4">a username</div>
-            <div className="grid-item mx-4"></div>
-            <div className="grid-item mx-4"></div>
-            <div className="grid-item mx-4 my-4"></div>
-            <div className="grid-item mx-4 my-4"></div>
-            <div className="grid-item mx-4 my-4"></div>
-            <div className="grid-item mx-4 font-bold">First Name</div>
-            <div className="grid-item mx-4 font-bold">Last Name</div>
-            <div className="grid-item mx-4 font-bold">Password</div>
-            <div className="grid-item mx-4">a first name</div>
-            <div className="grid-item mx-4">a last name</div>
-            <div className="grid-item mx-4">a password</div>
+    <tbody>
+      {users.map((user: User) => (
+        <tr key={user.user_id} className="my-44">
+          <div>
+            <p className="text-black mx-16 font-bold text-lg">
+              Hello, {user.first_name}!
+            </p>
+            <div className="my-16">
+              <div className="grid-container grid grid-cols-3 mx-96 border-2 p-2 border-black text-black text-lg">
+                <div className="grid-item mx-4 font-bold">Username</div>
+                <div className="grid-item mx-4"></div>
+                <div className="grid-item mx-4"></div>
+                <div className="grid-item mx-4">{user.username}</div>
+                <div className="grid-item mx-4"></div>
+                <div className="grid-item mx-4"></div>
+                <div className="grid-item mx-4 my-4"></div>
+                <div className="grid-item mx-4 my-4"></div>
+                <div className="grid-item mx-4 my-4"></div>
+                <div className="grid-item mx-4 font-bold">First Name</div>
+                <div className="grid-item mx-4 font-bold">Last Name</div>
+                <div className="grid-item mx-4 font-bold">Password</div>
+                <div className="grid-item mx-4">{user.first_name}</div>
+                <div className="grid-item mx-4">{user.last_name}</div>
+                <div className="grid-item mx-4">{censorPassword({user.password})}</div>
+              </div>
+              <div className="flex row-wrap flex-end mx-96">
+                <div className="my-2">
+                  <Button
+                    type="button"
+                    title="Account Settings"
+                    variant="btn_dark_green"
+                  />
+                </div>
+                <div className="p-2">
+                  <Button
+                    type="button"
+                    title="Wishlist"
+                    variant="btn_dark_green"
+                  ></Button>
+                </div>
+                <div className="my-2">
+                  <Button
+                    type="button"
+                    title="My Blogs"
+                    variant="btn_dark_green"
+                  ></Button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="flex row-wrap flex-end mx-96">
-            <div className="my-2">
-              <Button
-                type="button"
-                title="Account Settings"
-                variant="btn_dark_green"
-              />
-            </div>
-            <div className="p-2">
-              <Button
-                type="button"
-                title="Wishlist"
-                variant="btn_dark_green"
-              ></Button>
-            </div>
-            <div className="my-2">
-              <Button
-                type="button"
-                title="My Blogs"
-                variant="btn_dark_green"
-              ></Button>
-            </div>
-          </div>
-        </div>
-      </div>
-    </main>
+        </tr>
+      ))}
+    </tbody>
   );
 };
 
