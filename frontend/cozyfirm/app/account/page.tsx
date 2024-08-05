389349e4e926357bd62c788a7b862aaf8bf4a5cd
RepiMinji <lancele195@gmail.com>
2024-08-05 15:54:40 -0700

    contact
    added contact page

--- frontend/cozyfirm/app/account/page.tsx
+++ frontend/cozyfirm/app/account/page.tsx
@@ -4,7 +4,6 @@ import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import Button from "../../components/buttons";
-import login from "../login/page";
 
 interface User {
   user_id: number;
