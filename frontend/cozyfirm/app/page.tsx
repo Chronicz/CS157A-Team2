c7406ed6476aaa26c436d6e28d38819ee1d19017
Chronicz <45631752+Chronicz@users.noreply.github.com>
2024-08-05 20:59:31 -0700

    Update Home Page

--- frontend/cozyfirm/app/page.tsx
+++ frontend/cozyfirm/app/page.tsx
@@ -3,32 +3,34 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import axios from "axios";
-import Button from "../components/buttons";
-import Login from "./login/page";
-import Account from "./account/page";
 
-interface User {
-  user_id: number;
-  username: string;
-  password: string;
-  first_name: string;
-  last_name: string;
+interface Furniture {
+  furniture_id: number;
+  furniture_name: string;
+  brand: string;
+  description: string;
+  length: number;
+  height: number;
+  width: number;
+  price: number;
+  material: string;
+  color: string;
 }
 
 const Home = () => {
-  const [users, setUsers] = useState([]);
+  const [furnitures, setFurnitures] = useState([]);
 
   useEffect(() => {
-    const fetchUsers = async () => {
+    const fetchFurnitures = async () => {
       try {
         const res = await axios.get("http://localhost:8000");
-        setUsers(res.data);
+        setFurnitures(res.data);
         console.log(res);
       } catch (err) {
         console.log(err);
       }
     };
-    fetchUsers();
+    fetchFurnitures();
   }, []);
 
   return (
@@ -37,20 +39,26 @@ const Home = () => {
         <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
           <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
             <tr>
-              <th className="py-3 px-6">Username</th>
-              <th className="py-3 px-6">First Name</th>
-              <th className="py-3 px-6">Last Name</th>
+              <th className="py-3 px-6">Furnitures</th>
+              <th className="py-3 px-6">Brand</th>
+              <th className="py-3 px-6">Description</th>
+              <th className="py-3 px-6">Material</th>
+              <th className="py-3 px-6">Color</th>
+              <th className="py-3 px-6">Price</th>
             </tr>
           </thead>
           <tbody>
-            {users.map((user: User) => (
+            {furnitures.map((furn: Furniture) => (
               <tr
-                key={user.user_id}
+                key={furn.furniture_id}
                 className="bg-white border-b dark:bg-gray-800 dark:border-gray-700"
               >
-                <td className="py-4 px-6">{user.username}</td>
-                <td className="py-4 px-6">{user.first_name}</td>
-                <td className="py-4 px-6">{user.last_name}</td>
+                <td className="py-4 px-6">{furn.furniture_name}</td>
+                <td className="py-4 px-6">{furn.brand}</td>
+                <td className="py-4 px-6">{furn.description}</td>
+                <td className="py-4 px-6">{furn.material}</td>
+                <td className="py-4 px-6">{furn.color}</td>
+                <td className="py-4 px-6">{furn.price}</td>
               </tr>
             ))}
           </tbody>
