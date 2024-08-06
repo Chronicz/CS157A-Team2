383f7e8d91bf94ef4ca89490930eab14ea8f19ab
Chronicz <45631752+Chronicz@users.noreply.github.com>
2024-08-05 22:23:29 -0700

    Update Footer.tsx

--- frontend/cozyfirm/components/Footer.tsx
+++ frontend/cozyfirm/components/Footer.tsx
@@ -1,6 +1,6 @@
 const footer = () => {
   return (
-    <footer className="absolute bottom-0 w-full flex items-center justify-center py-4 border-2">
+    <footer className="absolute bottom-0 w-full flex items-center justify-center py-4 border-2 z-30">
       CozyFirm @ 2024
     </footer>
   );
