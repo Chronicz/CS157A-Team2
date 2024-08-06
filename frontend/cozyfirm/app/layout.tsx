5d6b4a41943d34797c34dc2bfe5e571032dc4d04
PikaPiperPickedaPickledPepper <kethyakas@yahoo.com>
2024-08-05 22:28:44 -0700

    Fixed footer CSS

--- frontend/cozyfirm/app/layout.tsx
+++ frontend/cozyfirm/app/layout.tsx
@@ -19,7 +19,9 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <Navbar />
-        <main className="relative overflow-hidden h-90 flex justify-center items-start">{children}</main>
+        <main className="relative overflow-hidden flex justify-center items-start">
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
--- frontend/cozyfirm/components/Footer.tsx
+++ frontend/cozyfirm/components/Footer.tsx
@@ -1,9 +1,9 @@
 const footer = () => {
   return (
-    <footer className="absolute bottom-0 w-full flex items-center justify-center py-4 border-2">
+    <footer className="sticky bottom-0 w-full flex items-center justify-center py-4 border-2 z-50 bg-white">
       CozyFirm @ 2024
     </footer>
   );
-}
+};
 
 export default footer;
