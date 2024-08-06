a63f65151d898ec1878b5e09c202b68093e68fb3
RepiMinji <lancele195@gmail.com>
2024-08-05 22:09:55 -0700

    browse

---
+++ frontend/cozyfirm/app/browse/page.tsx
@@ -0,0 +1,75 @@
+import React from "react";
+
+const page = () => {
+  return (
+    <div>
+      <div className="border-2 border-black">
+        <input placeholder="Search"></input>
+      </div>
+      <div className="bg-yellow-100 justify-center">
+        <div>
+          <label className="container">
+            filter 1 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div>
+          <label className="container">
+            filter 2 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div>
+          <label className="container">
+            filter 3 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div>
+          <label className="container">
+            filter 4 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div>
+          <label className="container">
+            filter 5 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div>
+          <label className="container">
+            filter 6 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div>
+          <label className="container">
+            filter 7 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div>
+          <label className="container">
+            filter 8 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div>
+          <label className="container">
+            filter 9 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+        <div>
+          <label className="container">
+            filter 10 <input type="checkbox"></input>
+            <span className="checkmark"></span>
+          </label>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
