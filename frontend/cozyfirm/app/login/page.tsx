90a593afe0e8213fbf28cf519bfbc18c036d2794
RepiMinji <lancele195@gmail.com>
2024-08-09 20:19:49 -0700

    e
    added some stuff for login, making new branch to test stuff

--- frontend/cozyfirm/app/login/page.tsx
+++ frontend/cozyfirm/app/login/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useState } from "react";
 import axios from "axios";
+import { getSession, login, logout } from "../lib";
 
 function Login() {
   const [username, setUsername] = useState("");
